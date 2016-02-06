6dba92f233822c8c7dedb1af0518faa9ca333cd0
Darlan Alves <me@darlanalv.es>
2016-02-06 16:51:29 -0200

    chore: explicitly export values by name on CommonJS syntax (to aid on tooling)

--- build.template.js
+++ build.template.js
@@ -1,21 +1,24 @@
 (function(global) {
 
-	/* content goes here */
+    /* content goes here */
 
-	var Gisele = {
-		Model: Model,
-		Field: Field,
-		RelationField: RelationField
-	};
+    var Gisele = {
+        Model: Model,
+        Field: Field,
+        RelationField: RelationField
+    };
 
-	if (typeof define === 'function' && define.amd) {
-		define(function() {
-			return Gisele;
-		});
-	} else if (typeof module !== 'undefined' && module.exports) {
-		module.exports = Gisele;
-	} else {
-		global.Gisele = Gisele;
-	}
+    if (typeof define === 'function' && define.amd) {
+        define(function() { return Gisele; });
+    } else if (typeof module !== 'undefined' && module.exports) {
+        // explicitly export values so static analysis tools can
+        // pick up the names
+
+        module.exports.Model = Model;
+        module.exports.Field = Field;
+        module.exports.RelationField = RelationField;
+    } else {
+        global.Gisele = Gisele;
+    }
 
 })(this);
